8df43590060fdbc391e25711096c0e9f2e19cefc
skaypist <skaypist@gmail.com>
2022-07-11 02:09:10 -0400

    removing unused

--- lib/utils.rb
+++ lib/utils.rb
@@ -17,36 +17,6 @@ module Utils
     @args = $gtk.args
   end
 
-  def to_json(obj, **opts)
-    defaults = {
-      indent: 4,
-      keep_sym: false
-    }
-    opts = defaults.merge opts
-
-    case obj
-    when Array
-      return "[]" if obj.empty?
-      str = obj.map { |v| to_json(v, **opts) }
-      return "[\n#{str.join(",\n")}".indent_lines(opts[:indent]) << "\n]" if opts[:indent]
-      "[#{str.join ","}]"
-    when Hash
-      return "{}" if obj.empty?
-      str = obj.map { |k, v| "#{to_json(k, **opts)}: #{to_json(v, **opts)}" }
-      return "{\n#{str.join(",\n")}".indent_lines(opts[:indent]) << "\n}" if opts[:indent]
-      "{#{str.join ","}}"
-    when String
-      obj.quote
-    when Symbol
-      return obj.inspect.quote if opts[:keep_sym]
-      obj.to_s.quote
-    when NilClass
-      'null'
-    else
-      obj.to_s
-    end
-  end
-
   module Flat
     def self.build(**kvs)
       Struct.
